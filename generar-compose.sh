2e4bf28fa855eaf3d206427d1b5ca06e86c54bed
bstarnone <bstarnone@fi.uba.ar>
2025-08-26 14:45:32 -0300

    fix indentation

--- generar-compose.sh
+++ generar-compose.sh
@@ -8,12 +8,12 @@ services:
   server:
   container_name: server
   image: server:latest
-    entrypoint: python3 /main.py
-    environment:
-      - PYTHONUNBUFFERED=1
-      - LOGGING_LEVEL=DEBUG
-    networks:
-      - testing_net
+  entrypoint: python3 /main.py
+  environment:
+    - PYTHONUNBUFFERED=1
+    - LOGGING_LEVEL=DEBUG
+  networks:
+    - testing_net
 EOL
 for i in $(seq 1 $client_number);
 do

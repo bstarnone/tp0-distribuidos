6cb760fe5c156e1e64a88831513e39fbb70b1b7a
bstarnone <bstarnone@fi.uba.ar>
2025-08-30 09:45:43 -0300

    fix quoting

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-RESPUESTA=$(docker run --rm --network=tp0_testing_net alpine /bin/sh -c "echo "mensaje" | nc server 12345")
+RESPUESTA=$(docker run --rm --network=tp0_testing_net alpine /bin/sh -c 'echo "mensaje" | nc server 12345')
 
-if ["$RESPUESTA" == "mensaje"];
+if ["$RESPUESTA" == 'mensaje'];
 then
   echo "bien"
 else

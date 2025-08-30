4d7eb6844cc858090be7ec7d3b4f873058b705b9
bstarnone <bstarnone@fi.uba.ar>
2025-08-30 10:10:39 -0300

    fix syntax for single bracket comparison

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 RESPUESTA=$(docker run --rm --network=tp0_testing_net alpine /bin/sh -c 'echo "mensaje" | nc server 12345')
 
-if [ "$RESPUESTA" == "mensaje" ]; then
+if [ "$RESPUESTA" = "mensaje" ]; then
   echo "action: test_echo_server | result: success"
 else
   echo "action: test_echo_server | result: fail"

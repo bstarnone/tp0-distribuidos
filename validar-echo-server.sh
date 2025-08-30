75c76c0bd3c949b708d08d07826727780aa89884
bstarnone <bstarnone@fi.uba.ar>
2025-08-30 09:43:14 -0300

    fix syntax

---
+++ validar-echo-server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+RESPUESTA=$(docker run --rm --network=tp0_testing_net alpine /bin/sh -c "echo 'mensaje' | nc server 12345")
+
+if ["$RESPUESTA" == "mensaje"];
+then
+  echo "bien"
+else
+  echo "mal"
+fi

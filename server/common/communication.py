0e915d43ff5782562b5757b0ee700969f8356105
bstarnone <bstarnone@fi.uba.ar>
2025-09-02 18:49:33 -0300

    remove struct library from protocol

--- server/common/communication.py
+++ server/common/communication.py
@@ -6,7 +6,7 @@ def consume_socket_data(client_sock):
     # print(f"leo raw len {raw_len}")
     if not raw_len:
         return -1
-    msg_len = struct.unpack(">H", raw_len)[0] #usa BigEndian (>I) para convertir los 2 bytes a un entero
+    msg_len = (raw_len[0] << 8) | raw_len[1]
     msg = b""
     while len(msg) < msg_len: #evitando short-reads
         data_rcv = client_sock.recv(msg_len - len(msg))
@@ -20,7 +20,11 @@ def send_client_bet_response(client_sock, dni: str, num: str):
     msg = f"{dni};{num}"
     msg_bytes = msg.encode("utf-8")
     msg_len = len(msg_bytes)
+    buf = bytes([
+        (msg_len >> 8) & 0xFF,
+        msg_len & 0xFF
+    ])
 
-    client_sock.sendall(struct.pack(">I", msg_len))
+    client_sock.sendall(buf)
     print("DEBUG-ENVIO ", msg_len, " bytes")
     client_sock.sendall(msg_bytes)
